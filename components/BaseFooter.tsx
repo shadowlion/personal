44f4d6c2dc040565ec8a49d89d1a66537e457ce6
Justin Chiou <chiou.kai@gmail.com>
2021-08-28 13:39:42 -0400

    Add hero icons to components

--- components/BaseFooter.tsx
+++ components/BaseFooter.tsx
@@ -1,12 +1,15 @@
-const BaseFooter = () => {
-  const year = new Date().getFullYear();
+import { CodeIcon, HeartIcon } from "@heroicons/react/outline";
 
+const BaseFooter = () => {
   return (
     <div className="mt-12 lg:mt-18 sm:py-12 py-6">
       <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
         <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div>
         <div className="flex flex-col justify-between lg:flex-row items-center">
-          <p>&copy; Justin Chiou | {year} | All Rights Reserved.</p>
+          <div className="inline-flex items-center">
+            <CodeIcon className="h-5 w-5 mr-1" /> with{" "}
+            <HeartIcon className="h-5 w-5 mx-1" /> since 2017.
+          </div>
           <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
             <a
               href="https://www.linkedin.com/in/justinchiou"
